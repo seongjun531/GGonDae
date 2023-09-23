ac6a134221727441ef9545b33e3baed98cd47669
MaEulHP <66859688+MaEulHP@users.noreply.github.com>
2023-09-23 13:53:00 +0900

    AI Trace Update

--- GGonDae/Assets/Script/EnemyAI.cs
+++ GGonDae/Assets/Script/EnemyAI.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public int nextdX, nextdY;
+    int nextdX, nextdY;
     public Rigidbody2D rb;
-    public float moveSpeed = 1f;
+    public float traceSpeed = 1.5f;
     public float Hp = 1000.0f;
+    bool isMoving = true;
+    bool isTracing = false;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        Direction();
+        Invoke("Direction", 2);
     }
     void FixedUpdate()
     {
@@ -20,9 +22,45 @@ public class EnemyAI : MonoBehaviour
     }
     void Direction()
     {
-        nextdX = Random.Range(-2, 3);
-        nextdY = Random.Range(-2, 3);
-        rb.velocity = new Vector2(nextdX, nextdY);
-        Invoke("Direction", 2);
+        if (isMoving){
+            nextdX = Random.Range(-2, 3);
+            nextdY = Random.Range(-2, 3);
+            rb.velocity = new Vector2(nextdX, nextdY);
+            Invoke("Direction", 2);
+        }
+
+        else if(isTracing) {
+            Vector2 playerPos = traceTarget.transform.position;
+            if (playerPos.x < transform.position.x){
+                rb.velocity = new Vector2(-traceSpeed, rb.velocity.y);
+            }
+
+            else if(playerPos.x > transform.position.x){
+                rb.velocity = new Vector2(traceSpeed, rb.velocity.y);
+            }
+
+            if (playerPos.y < transform.position.y){
+                rb.velocity = new Vector2(rb.velocity.x, -traceSpeed);
+            }
+
+            else if(playerPos.y > transform.position.y){
+                rb.velocity = new Vector2(rb.velocity.x, traceSpeed);
+            }
+        }
+        
+    }
+    void OnTriggerEnter2D(Collider other)
+    {
+        if (other.gameObject.tag == "Player"){
+            traceTarget - other.gameObject;
+
+            isMoving = false;
+        }
+    }
+    void OnTriggerStay2D(Collider other)
+    {
+        if (other.gameObject.tag == "Player"){
+            isTracing = true;
+        }
     }
 }
